c1ff5c2d01bbd15090efe1af5298cb635697ea40
kajprintz <kajprintz@gmail.com>
2024-10-31 00:53:23 +0100

    update

--- shared/set-kitty-font-size.sh
+++ shared/set-kitty-font-size.sh
@@ -1,15 +1,17 @@
+KITTY_CONF_PATH="$HOME/.config/kitty/kitty.conf"
+
 choose_font_size() {
   if [ -n "$1" ]; then
     choice="$1"
   else
     # Read the current font size and ask user to pick one
-    current_font_size=$(grep -Po '(?<=^font_size )\d+' $HOME/.config/kitty/kitty.conf)
+    current_font_size=$(grep -Po '(?<=^font_size)\s*\d+' $HOME/.config/kitty/kitty.conf)
     choice=$(gum choose {7..16} "> Skip" --selected $current_font_size --height 14 --header "Choose your terminal font size")
   fi
 
   # Update kitty config
   if [[ $choice =~ ^[0-9]+$ ]]; then
-    sed -i "s/^font_size .*$/font_size $choice/g" $HOME/.config/kitty/kitty.conf
+    sed -i -e "s/font_size .*/font_size  ${choice:-10}/g" $KITTY_CONF_PATH
   fi
 }
 
--- shared/set-kitty-font.sh
+++ shared/set-kitty-font.sh
@@ -8,13 +8,4 @@ fi
 
 font_family="$1"
 
-# Remove old font configuration lines globally
-sed -i '/^# BEGIN_KITTY_FONTS/d' $KITTY_CONF_PATH
-sed -i '/^font_family/d' $KITTY_CONF_PATH
-sed -i '/^bold_font/d' $KITTY_CONF_PATH
-sed -i '/^italic_font/d' $KITTY_CONF_PATH
-sed -i '/^bold_italic_font/d' $KITTY_CONF_PATH
-sed -i '/^# END_KITTY_FONTS/d' $KITTY_CONF_PATH
-
-# Append the new font configuration at the end of the file
-echo -e "\n# BEGIN_KITTY_FONTS\nfont_family      family=\"$font_family\"\nbold_font        auto\nitalic_font      auto\nbold_italic_font auto\n# END_KITTY_FONTS\n" >>$KITTY_CONF_PATH
+sed -i -e "s/font_family .*/font_family      $font_family/g" $KITTY_CONF_PATH
