75e411281afcfbbb88a7454b41efaa07368d07a7
kajprintz <kajprintz@gmail.com>
2024-11-03 14:37:26 +0100

    update

--- bin/scripts/install-mise.sh
+++ bin/scripts/install-mise.sh
@@ -1,5 +1,5 @@
 # Install Mise version control manager
 
-cmd="sudo dnf install -y dnf-plugins-core && sudo dnf config-manager --add-repo https://mise.jdx.dev/rpm/mise.repo && sudo dnf install -y mise"
+cmd="sudo dnf install -y dnf-plugins-core && sudo dnf config-manager addrepo https://mise.jdx.dev/rpm/mise.repo && sudo dnf install -y mise"
 
 $OMARA_PATH/shared/gum-spinner.sh "$cmd" "Installing Mise..."
